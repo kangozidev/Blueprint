020ff65157a199f733e1e0f510622eb448401428
Oz <57510416+kangozidev@users.noreply.github.com>
2025-04-10 13:16:16 +0700

    Update __init__.py

--- Pertanian/SmartFarm Monitor/Project/smartfarm_server/app/ml/__init__.py
+++ Pertanian/SmartFarm Monitor/Project/smartfarm_server/app/ml/__init__.py
@@ -1,1 +1,7 @@
 
+# smartfarm_server/app/ml/__init__.py
+
+# File ini bisa kosong.
+# Menandakan direktori 'ml' sebagai paket Python.
+# Di sini bisa juga ditambahkan inisialisasi level paket jika diperlukan,
+# misalnya logging khusus untuk modul ML.
